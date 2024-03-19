f5990f692cd0411d2a61b73cb12e18fd5ebeae97
Luca <luca@ahd-creative.agency>
2024-03-19 20:36:32 +0100

    feat(components): logger

---
+++ src/log.ts
@@ -0,0 +1,10 @@
+import {BaseCommandOptions} from "./types";
+
+/**
+ * Returns, in this order, the info, warn and error settings for logging.
+ *
+ * @param options an object extending `BaseCommandOptions`
+ */
+export function getLoggerSettingsFromOptions(options: Partial<BaseCommandOptions>) {
+    return [options?.useConsoleLog ?? true, options?.useConsoleLog ?? true, options?.useConsoleLog ?? true];
+}
