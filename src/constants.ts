6fe788b794c6330bb1a4c0e34ced64c23c45ec6e
Luca <luca@ahd-creative.agency>
2024-03-20 15:09:07 +0100

    feat(components): constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SINGLE_MODULE_MANIFEST = 'module.json';
+export const MODULES_MANIFEST = 'modules.json';
+export const INSTALL_MANIFEST = 'install.json';
+export const FULL_FILE = 'full.zip';
+export const BASE_FILE = 'base.zip';
+export const DEFAULT_SPLIT_FILE_SIZE = 1_073_741_824;
